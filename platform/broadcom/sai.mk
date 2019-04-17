97be499a113efc75c3b696233ac68411ac218467
Shu0T1an ChenG <shuche@microsoft.com>
2019-04-17 01:40:56 -0700

    update sai package

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
 BRCM_SAI = libsaibcm_3.5.1.1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.5.1.1_amd64.deb?sv=2015-04-05&sr=b&sig=ssvwfKFyGbPLzTTs3g%2Bhk7%2FNvziRDnUpqeHVmaDNQGQ%3D&se=2032-12-23T00%3A19%3A05Z&sp=r"
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.5.1.1_amd64.deb?sv=2015-04-05&sr=b&sig=CN%2FiUCbfnQ6BurhfxsnNolE8exFizhiZPrRUpQo6WPk%3D&se=2032-12-24T08%3A39%3A47Z&sp=r"
 
 BRCM_SAI_DEV = libsaibcm-dev_3.5.1.1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.5.1.1_amd64.deb?sv=2015-04-05&sr=b&sig=ZX0Qry2r3f2eFOWn%2Ff1Q3UOnz1QtljtFs0vYvl2%2BiEk%3D&se=2032-12-23T00%3A18%3A28Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.5.1.1_amd64.deb?sv=2015-04-05&sr=b&sig=cjamCSm%2FjRc%2BeFBK0fTnvqRX9B79Hv6znUn9r3YK9iw%3D&se=2032-12-24T08%3A40%3A26Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
